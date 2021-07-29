cf005e0092b2a5b39f6f58adbad8cca736fe0479
Wariscan Kit <wariscan.kit@gmail.com>
2021-07-29 22:11:27 +0800

    Add another sample ImageTarget
    ImageTargets from a notes.

---
+++ kit-ar/Assets/Scripts/AutoRotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoRotate : MonoBehaviour {
+
+	// Use this for initialization
+	public float speed = 10f;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Rotate (Vector3.up * speed * Time.deltaTime);
+	
+	}
+}
