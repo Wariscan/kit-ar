f0aec203cb9c336c8531e04aec54badc47d66157
i.am.AR <shamsulamar@icloud.com>
2025-01-13 20:50:57 +0800

    Vuforia framework

--- Assets/ARFoundationHelper/Scripts/ARFoundationHelperSettings.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ARFoundationHelperSettings : ScriptableObject
-{
-    public string GeneratedLibrarySavePath = "Assets/ARFoundationHelper/GeneratedLibrary/auto-image-lib.asset";
-    public Material DefaultImageTrackerMaterial;
-}
